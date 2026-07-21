d312018ae6c2ff20794df113b312dbc8462b448c
Kothala Prem sai <160343916+premjohnson@users.noreply.github.com>
2026-07-21 20:14:01 +0530

    Add comment for MongoDB disconnection handling
    Added a comment for disconnection handling in MongoDB.

--- config/connectDB.js
+++ config/connectDB.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGODB_URI);
 
         console.log("MongoDB Connected");
-
+        //disconnection
         mongoose.connection.on("disconnected", () => {
             console.log("MongoDB Disconnected");
         });
